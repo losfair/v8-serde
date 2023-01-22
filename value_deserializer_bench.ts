6b6f90156a00b2620c4e845508e9a9946b41c1d4
losfair <zhy20000919@hotmail.com>
2023-01-22 15:12:58 +0800

    add more benchmarks

--- value_deserializer_bench.ts
+++ value_deserializer_bench.ts
@@ -8,10 +8,51 @@ const deserializeNative: (x: unknown) => Uint8Array = Deno.core.deserialize;
 
 {
   const payload = serialize(false);
+  const json = JSON.stringify(false);
   Deno.bench(function deserializeSmallJS() {
     deserialize(payload);
   });
   Deno.bench(function deserializeSmallNative() {
+    deserializeNative(payload);
+  });
+  Deno.bench(function deserializeSmallJSON() {
+    JSON.parse(json);
+  });
+}
+
+{
+  const obj = Object.fromEntries(
+    Array(10000).fill(0).map((_x, i) => ["a" + i, i]),
+  );
+  const payload = serialize(obj);
+  const json = JSON.stringify(obj);
+  Deno.bench(function deserializeLargeJS() {
+    deserialize(payload);
+  });
+  Deno.bench(function deserializeLargeNative() {
+    deserializeNative(payload);
+  });
+  Deno.bench(function deserializeLargeJSON() {
+    JSON.parse(json);
+  });
+}
+
+{
+  const obj = {
+    a: {
+      b: null as unknown,
+    },
+    b: {
+      a: null as unknown,
+    },
+  };
+  obj.a.b = obj.b;
+  obj.b.a = obj.a;
+  const payload = serialize(obj);
+  Deno.bench(function deserializeCircularJS() {
+    deserialize(payload);
+  });
+  Deno.bench(function deserializeCircularNative() {
     deserializeNative(payload);
   });
 }
